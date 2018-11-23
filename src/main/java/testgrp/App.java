5875d4d80b8e729bda91187f75a891af13102592
Igor Blackman <igor.blackman@gmail.com>
2018-11-23 18:18:44 -0200

    Add comments

--- src/main/java/testgrp/App.java
+++ src/main/java/testgrp/App.java
@@ -14,9 +14,12 @@ public class App
     private static final Logger logger = (Logger) LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
+    	// Mapped Diagnostic Context 
     	MDC.put("key", "value");
-        System.out.println( "Hello World!" );
+    	//comparison between a normal message and a message using the logstash-logback encoder
+        System.out.println("Debug message");
         logger.debug("Debug message");
+        //update MDC "key" value
         MDC.put("key", "value2");
         logger.debug("Debug message 2");
         MDC.clear();
