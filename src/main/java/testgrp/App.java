05f62dfc2696fd534e2d4abb9f6be77878e0425c
Igor Blackman <igor.blackman@gmail.com>
2018-11-23 18:14:21 -0200

    Add MDC use

--- src/main/java/testgrp/App.java
+++ src/main/java/testgrp/App.java
@@ -1,6 +1,7 @@
 package testgrp;
 
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 import ch.qos.logback.classic.Logger;
 
@@ -13,7 +14,11 @@ public class App
     private static final Logger logger = (Logger) LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
+    	MDC.put("key", "value");
         System.out.println( "Hello World!" );
         logger.debug("Debug message");
+        MDC.put("key", "value2");
+        logger.debug("Debug message 2");
+        MDC.clear();
     }
 }
